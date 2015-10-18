374910aafba336aff69257a8163141a38e4d9470
Shayol <vorga88@gmail.com>
2015-10-18 13:03:11 +0300

    add facebook login with token that needs fixing

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -209,6 +209,16 @@ myApp.controller('LoginCtrl', function($scope, $auth, $state, toaster) {
   $scope.login = function() {
       $auth.submitLogin($scope.loginForm);
     };
+  $scope.authenticate = function() {
+    $auth.authenticate('facebook')
+      .then(function(resp) {
+        toaster.success("Logged in with Facebook.");
+        $state.go("/projects");
+      })
+      .catch(function(resp) {
+
+      });
+  };
 });
 
 myApp.controller('SignupCtrl', function($scope, $auth, $state, toaster) {
