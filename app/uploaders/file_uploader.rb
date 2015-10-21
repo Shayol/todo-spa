023ec5c96e8a4a266bfb5105fcca75d4e93060c7
Shayol <thesadstorm@yandex.ua>
2015-10-21 14:36:47 -0400

    add env condition to uploads

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -7,14 +7,19 @@ class FileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :dropbox
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  if Rails.env.test? or Rails.env.development?
+    storage :file
+  else
+    storage :dropbox
   end
+
+def store_dir
+  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+end
+
+  
+
+  # storage :fog
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
