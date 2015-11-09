9fdf1ed6412f28a4629f261b944c204628d24859
Shayol <thesadstorm@yandex.ua>
2015-11-09 20:13:30 +0200

    change project controller js spec

---
+++ spec/javascripts/controllers/new-project_spec.js
@@ -0,0 +1,19 @@
+describe('NewProjectCtrl', function() {
+  beforeEach(module('myApp'));
+
+  //var $controller;
+  var $httpBackend = null;
+
+  beforeEach(inject(function(_$controller_, _$httpBackend_){   
+    $controller = _$controller_;
+    $httpBackend = _$httpBackend_;
+    //$state = _$state_;
+  }));
+ 
+
+  it ("has $scope.addProject method", function() {
+    var scope = {};
+    var ctrl = $controller('NewProjectCtrl', $scope: scope);
+    expect($scope.addProject).toBeDefined();
+  });
+});
